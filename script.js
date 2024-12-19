ea237bab27828b96981127ea7501901fb8122864
Rakesh <144416643+Rakesh211212@users.noreply.github.com>
2024-12-19 15:43:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+
 function showMessage() {
     alert("Welcome to XYZ College! Explore our website to learn more.");
 }
+
+
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
+    
     document.getElementById("formMessage").innerText = `Thank you, ${name}! We have received your message and will get back to you soon.`;
-    this.reset(); // Reset the form fields
+
+    this.reset();
 });
